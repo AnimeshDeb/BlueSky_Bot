dafa992d2d8225b110f0bb525e8e1518fb01f8d5
MystProgrammer <animeshdeb89@gmail.com>
2025-01-13 19:16:23 -0500

    Landing page simple design and initial backend setup

---
+++ client/src/Routes/Home.jsx
@@ -0,0 +1,20 @@
+import '../styles/home.css';
+
+function SignIn() {
+  return (
+    <div className='container'>
+        <div className='home-left'>
+PLACE HOLDER TEXT
+        </div>
+      <div className='home'>
+        <button className='btn'>Login with existing Bluesky credentials</button>
+        <button className='btn'>Need to make a new Bluesky account</button>
+      </div>
+      <div className='home-right'>
+PLACEHOLDER TEXT
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
